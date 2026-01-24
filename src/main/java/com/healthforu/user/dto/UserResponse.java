19beffe708e4c3ed5001034a7efe8d1a81d8c5b6
rudduddl <kim5472678@gmail.com>
2026-01-24 23:43:23 +0900

    feat : user 식별 정보 추가

--- src/main/java/com/healthforu/user/dto/UserResponse.java
+++ src/main/java/com/healthforu/user/dto/UserResponse.java
@@ -3,12 +3,14 @@ package com.healthforu.user.dto;
 import com.healthforu.user.domain.User;
 
 public record UserResponse(
+        String id,
         String loginId,
         String userName
 ) {
     /** 엔티티를 응답 DTO로 변환 */
     public static UserResponse from(User user){
         return new UserResponse(
+                user.getId().toHexString(),
                 user.getLoginId(),
                 user.getUserName()
         );

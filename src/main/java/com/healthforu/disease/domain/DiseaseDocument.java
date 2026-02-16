ff55a60e0b072122276b79035c91f2f1e135a486
rudduddl <kim5472678@gmail.com>
2026-02-17 01:51:19 +0900

    feat: elastic setting 어노테이션 추가

--- src/main/java/com/healthforu/disease/domain/DiseaseDocument.java
+++ src/main/java/com/healthforu/disease/domain/DiseaseDocument.java
@@ -8,12 +8,14 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(indexName = "diseases")
+@Setting(settingPath = "elasticsearch/settings.json")
 public class DiseaseDocument {
 
     @Id
